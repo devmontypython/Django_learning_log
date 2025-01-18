4a97ed7fcd0bf0f0b08e4acadf537b74fdf53d9d
devmontypython <devmontypython>
2025-01-18 18:11:00 -0300

    Registrando Entry junto ao site de administração

--- applearning_logs/admin.py
+++ applearning_logs/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 #Para registrar Topic junto ao site de administração, digite:
-from applearning_logs.models import Topic
+from applearning_logs.models import Topic, Entry
 admin.site.register(Topic)
-#admin.site.register()
+admin.site.register(Entry)
 
 # Register your models here.
