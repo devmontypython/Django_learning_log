3fbd7c501dd7e1caa79bf810c28abfcbd6e871a5
devmontypython <devmontypython>
2025-01-18 17:17:53 -0300

    Alguns acertos.

--- applearning_logs/admin.py
+++ applearning_logs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+#Para registrar Topic junto ao site de administração, digite:
+from applearning_logs.models import Topic
+admin.site.register(Topic)
+#admin.site.register()
 
 # Register your models here.
